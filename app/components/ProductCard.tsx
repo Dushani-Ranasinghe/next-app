118f0f837d8c2e37202e190404ff9b4e6c5c616e
Dushani-Ranasinghe <uththaradushani@gmail.com>
2025-01-02 23:43:40 +0530

    styling

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -3,10 +3,22 @@ import AddToCart from './AddToCart'
 
 const ProductCard = () => {
   return (
-    <div>
+    <div className='p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-600'>
         <AddToCart/>
     </div>
   )
 }
 
 export default ProductCard
+
+// import React from 'react'
+// import AddToCart from './AddToCart'
+// import styles from './ProductCard.module.css'
+// const ProductCard = () => {
+//   return (
+//     <div className={styles.card}>
+//         <AddToCart/>
+//     </div>
+//   )
+// }
+// export default ProductCard
